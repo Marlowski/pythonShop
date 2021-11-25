081ab8675badc9f49556a54711719f3cda2a6720
leon_meinl <leon.ww@web.de>
2021-11-25 21:00:24 +0100

    Commit signup und login templates

--- django_settings/urls.py
+++ django_settings/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_page, name='landing-page'),
+    path('userManagement/', include('UserManagement.urls')),
+    path('userManagement/', include('django.contrib.auth.urls'))
 ]
